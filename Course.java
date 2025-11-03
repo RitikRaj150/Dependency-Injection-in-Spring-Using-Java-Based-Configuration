903514651058437ef8ee2ed48da2a36991fef548
Ritik_Raj <ritikraj91428@gmail.com>
2025-11-03 10:50:50 +0530

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,13 @@
+package com.nimbus.di;
+
+public class Course {
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
